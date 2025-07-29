62062b900b6b23a461bdf2cbc0931eb613dd4362
YniRus <YniRus@yandex.ru>
2025-07-29 12:20:10 +0300

    update rclone to get RCLONE_REMOTE_NAME from .env

--- rclone/clone.sh
+++ rclone/clone.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-RCLONE_REMOTE_NAME="gdrive"
+HOME_DIR=$(eval echo ~)
+
+if [ -f "${HOME_DIR}/pert-estimating-composer/.env" ]; then
+    source "${HOME_DIR}/pert-estimating-composer/.env"
+else
+    echo "Error: File .env is not found"
+    exit 1
+fi
+
+if [ -z "$RCLONE_REMOTE_NAME" ]; then
+  echo "Error: RCLONE_REMOTE_NAME environment variable is not set"
+  exit 1
+fi
 
 DATE=$(date +"%d.%m.%Y")
 
 echo "Starting backup process for $DATE at $(date)"
 
 echo "Backing up server-storage..."
-rclone copy ~/pert-estimating-composer/server-storage "${RCLONE_REMOTE_NAME}:rclone/pert-estimating/$DATE/server-storage"
+rclone copy "${HOME_DIR}/pert-estimating-composer/server-storage" "${RCLONE_REMOTE_NAME}:rclone/pert-estimating/$DATE/server-storage"
 
 echo "Backing up server-assets..."
-rclone copy ~/pert-estimating-composer/server-assets "${RCLONE_REMOTE_NAME}:rclone/pert-estimating/$DATE/server-assets"
+rclone copy "${HOME_DIR}/pert-estimating-composer/server-assets" "${RCLONE_REMOTE_NAME}:rclone/pert-estimating/$DATE/server-assets"
 
 echo "Backup $DATE completed at $(date)"
